09bd169faf22c911c9109aa4f36eba71cbfbf9ff
Zhumabai00 <asanaalyev1@gmail.com>
2024-01-14 15:52:15 +0700

    auth

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import { useForm } from 'react-hook-form'
+import styles from "./Login.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
+import { Navigate } from "react-router-dom";
+
+export const Login = () => {
+  const isAuth = useSelector(selectIsAuth)
+  const dispatch = useDispatch();
+
+  const { register, handleSubmit, setError, formState: { errors, isValid } } = useForm({
+    defaultValues: {
+      email: 'asanaa@gmail.com',
+      password: '12345',
+    },
+    mode: 'onChange'
+  })
+
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values));
+  }
+  if (isAuth) {
+    return <Navigate to={'/'} />
+  }
+
+  return (
+    <Paper classes={{ root: styles.root }}>
+      <Typography classes={{ root: styles.title }} variant="h5">
+        Вход в аккаунт
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          type="email"
+          error={!!errors.email?.message}
+          helperText={errors.email?.message}
+          {...register('email', { required: 'Write an email!' })}
+          fullWidth
+        />
+        <TextField
+          type='password'
+          className={styles.field}
+          error={!!errors.password?.message}
+          label="Пароль"
+          helperText={errors.password?.message}
+          fullWidth {...register('password', { required: 'Enter the password!' })}
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
+    </Paper>
+  );
+};
